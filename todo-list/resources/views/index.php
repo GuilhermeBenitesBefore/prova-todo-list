c991b1c5c10a8f86f54a83f5602df11f586a7392
Caio Colaiacovo <caiohshot@gmail.com>
2017-12-02 00:22:41 -0300

    First prototype of layout

---
+++ todo-list/resources/views/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <link rel="stylesheet" href="/css/app.css?p=12312312">
+  <title>TODO List</title>
+</head>
+<body>
+  <div class="container" style="max-width: 800px !important">
+
+
+    <div class="row">
+      <h3>Prova Before <small>- 01/01/2017</small></h3>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <label>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis eleifend tortor. 
+          Aenean sit amet est elementum, rutrum ipsum at, eleifend est. Suspendisse id sollicitudin urna. 
+          Fusce ornare viverra sem in consectetur.
+        </label>
+      <div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <button type="button" class="btn btn-primary btn-xs">Categoria</button>
+        <i class="fa fa-calendar" aria-hidden="true"></i> 01/01/2017
+        <i class="fa fa-flag-checkered" aria-hidden="true"></i> 01/01/2017
+        <p class="pull-right">
+          <i class="fa fa-pencil-square-o fa-2x" aria-hidden="true"></i>
+        </p>
+      </div>
+    </div>
+    
+    
+
+
+  </div>
+</body>
+</html>
