2c3359039d44ed972b3f48836b4b19741ae6acf3
Jean <jeanwfsantos@gmail.com>
2017-12-02 09:36:54 -0300

    inclusao da configuracao dos models e relacoes

---
+++ database/migrations/2017_12_02_122714_create_tag_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->integer('tag_id')->unsigned()->nullable();
+            $table->foreign('tag_id')->references('id')
+                ->on('tags')->onDelete('cascade');
+
+            $table->integer('task_id')->unsigned()->nullable();
+            $table->foreign('task_id')->references('id')
+                ->on('tasks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_task');
+    }
+}
