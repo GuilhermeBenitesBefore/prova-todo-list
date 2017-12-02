f66340fee5763b612f0b1bb3d5575475cf9e1e56
Marlom Souza <thunder_bolt2000@hotmail.com>
2017-12-02 16:20:04 -0300

    ajuste layout

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -1,36 +1,35 @@
 @extends('principal')
 @section('conteudo')
-    @foreach($toDos as $toDo)
 
-
+@foreach($toDos as $toDo)
 <div class="quote-container">
   <blockquote class="note yellow">
     <input type="text" class="form-control" id="tituloTodo" 
-            name="tituloTodo" placeholder="Título" value="{{$toDo->titulo}}" >
-    <textarea  style="overflow:hidden" class="form-control" rows="5" placeholder="Descrição">
-    {{$toDo->descricao}}
+      name="tituloTodo" placeholder="Título" value="{{$toDo->titulo}}" >
+    <textarea  style="overflow:hidden" class="form-control" rows="5" placeholder="Descrição">   {{$toDo->descricao}}
     </textarea>
     <cite class="botaoLembrar">
-    <div class="row">
-      <div class="form-check">
-      <label for="tituloTodo">Validade: </label>
-      <label class="form-check-label" >
-        <input type="checkbox" class="form-check-input">1 ano
-        </label>
-        <label class="form-check-label" >
-        <input type="checkbox" class="form-check-input">1 mês
-        </label>
-        <label class="form-check-label" >
-        <input type="checkbox" class="form-check-input">1 dia
-        </label>
-        <label class="form-check-label" >
-        <input type="checkbox" class="form-check-input">1 Hora
-      </label>
-    </div>
-    </div>  
+      <div class="row">
+        <div class="form-check">
+          <label for="tituloTodo">Validade: </label>
+          <label class="form-check-label" >
+          <input type="checkbox" class="form-check-input">1 ano
+          </label>
+          <label class="form-check-label" >
+          <input type="checkbox" class="form-check-input">1 mês
+          </label>
+          <label class="form-check-label" >
+          <input type="checkbox" class="form-check-input">1 dia
+          </label>
+          <label class="form-check-label" >
+          <input type="checkbox" class="form-check-input">1 Hora
+          </label>
+        </div>
+      </div>
     </cite>
   </blockquote>
-    @endforeach
+</div>
+@endforeach
 
 @endsection
     
