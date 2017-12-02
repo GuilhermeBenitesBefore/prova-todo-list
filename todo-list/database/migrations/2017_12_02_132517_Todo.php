711cb28ed827545348156434652c06dad15ce9e9
Caio Colaiacovo <caiohshot@gmail.com>
2017-12-02 16:11:09 -0300

    Fix migrations

---
+++ todo-list/database/migrations/2017_12_02_132517_Todo.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Todo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->char('title',200);
+            $table->text('description');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
