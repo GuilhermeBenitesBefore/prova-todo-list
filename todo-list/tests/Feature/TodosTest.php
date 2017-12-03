b6eb05202efd43ff2ea065c0d8f8770996a33606
Caio Colaiacovo <caiohshot@gmail.com>
2017-12-03 16:47:47 -0300

    Test improvements; writing some more tests

--- todo-list/tests/Feature/TodosTest.php
+++ todo-list/tests/Feature/TodosTest.php
@@ -13,28 +13,74 @@ class TodosTest extends TestCase
 {
     use DatabaseMigrations;
     
-    public function testViewingAllTodos()
-    {
-        $category = factory(Category::class)->create([
+    public function setUp(){
+        parent::setUp();
+
+        $leasureCategory = factory(Category::class)->create([
             'id' => 1,
             'name' => 'Leasure'
             ]);
-        $category->save();
+        $leasureCategory->save();
 
-        $todo = factory(Todo::class)->create([
+        $workCategory = factory(Category::class)->create([
+            'id' => 2,
+            'name' => 'Work'
+            ]);
+        $workCategory->save();
+
+        $todoReservation = factory(Todo::class)->create([
             'id' => 1, 
-            'title' => 'My first todo', 
-            'description' => 'This is my first todo!',
-            'category_id' => $category->id,
+            'title' => 'Make reservation at restaurant.', 
+            'description' => 'Invite my parents, some friends from school and Caio.',
+            'category_id' => $leasureCategory->id,
             'deadline' => '2017-01-01'
             ]);
         
-        $todo->save();
+        $todoReservation->save();
 
-        $this->get('/')->assertSeeText('This is my first todo!');
+        $todoFixComputer = factory(Todo::class)->create([
+            'id' => 2, 
+            'title' => 'Fix my computer', 
+            'description' => 'Upgrade to Windows 10 and install Office.',
+            'category_id' => $workCategory->id,
+            'deadline' => '2017-01-01'
+            ]);
+        
+        $todoFixComputer->save();
+    }
+    public function tearDown(){
+        parent::tearDown();
     }
 
-    public function testShouldNotPersistInvalidTodo() {
-        $this->call('POST', '/add')->assertStatus(302);
+    public function testShouldNotPersistEmptyTodo() {
+        $response = $this->post('/add');
+
+        $response->assertSessionHasErrors('title');
+        $response->assertSessionHasErrors('description');
+        $response->assertSessionHasErrors('category_id');
+        $response->assertSessionHasErrors('deadline');
+    }
+
+    public function testViewingAllTodos() {
+        $response = $this->get('/');
+        $response->assertSeeText('Make reservation at restaurant.');
+        $response->assertSeeText('Upgrade to Windows 10 and install Office.');
+    }
+
+    public function testEditATodoWithValues() {
+        $newDescription = 'Upgrade to Windows 10, install Office and firefox.';
+
+        $todoToBeEdited = Todo::find(2);
+        $todoToBeEdited->description = $newDescription;
+            
+        $this->post('/update', $todoToBeEdited->toArray())->assertStatus(302);
+        $this->get('/')->assertSeeText($newDescription);
+    }
+
+    public function testShouldNotEditATodoWithoutTitle() {
+        $todoToBeEdited = Todo::find(1);
+        $todoToBeEdited->title = null;
+            
+        $this->post('/update', $todoToBeEdited->toArray())->assertSessionHasErrors('title');
     }
 }
