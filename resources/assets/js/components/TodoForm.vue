3a981d7f212978473d079b1520286943052e03db
Jean <jeanwfsantos@gmail.com>
2017-12-02 22:28:48 -0300

    inclusao do modulo vue2-simplert

--- resources/assets/js/components/TodoForm.vue
+++ resources/assets/js/components/TodoForm.vue
@@ -1,82 +1,88 @@
 <template>
-    <form>
-        <div class="form-group">
-            <label class="control-label" for="title">Title</label>
-            <input
-                type="text"
-                class="form-control"
-                id="title"
-                name="title"
-                placeholder="Title"
-                v-model="todo.title" />
-        </div>
-        <div class="form-group">
-            <label class="control-label" for="tags">Tags</label>
-            <v-select
-                multiple
-                :options="tags"
-                :taggable="true"
-                v-model="todo.tags"
-                label="title"></v-select>
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea
-                id="description"
-                class="form-control"
-                rows="3"
-                v-model="todo.description"></textarea>
-        </div>
-        <div class="form-group">
-            <label class="control-label" for="start">Start</label>
-            <datepicker
-                name="start"
-                format="dd/MM/yyyy"
-                input-class="form-control datepicker__input"
-                language="pt-br"
-                :required="true"
-                :disabled="startOptions"
-                @selected="handleStartChange"
-                v-model="todo.start"></datepicker>
-        </div>
-        <div class="form-group">
-            <label class="control-label" for="end">End</label>
-            <datepicker
-                name="end"
-                format="dd/MM/yyyy"
-                input-class="form-control datepicker__input"
-                language="pt-br"
-                :required="true"
-                :disabled="endOptions"
-                @selected="handleEndChange"
-                v-model="todo.end"></datepicker>
-        </div>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" v-model="todo.done"> Done
-            </label>
-        </div>
-        <button
-            type="button"
-            class="btn btn-primary"
-            v-if="!isEditMode"
-            @click.prevent="addTodo()">Submit</button>
-        <button
-            type="button"
-            class="btn btn-primary"
-            v-if="isEditMode"
-            @click.prevent="editTodo()">Submit</button>
-        <button
-            type="button"
-            class="btn btn-danger"
-            v-if="isEditMode"
-            @click="removeTodo()">Remove</button>
-        <button
-            type="button"
-            class="btn btn-default"
-            v-if="isEditMode"
-            @click="cancelEditMode()">Cancel</button>
-    </form>
+    <div>
+        <form>
+            <div class="form-group">
+                <label class="control-label" for="title">Title</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    id="title"
+                    name="title"
+                    placeholder="Title"
+                    v-model="todo.title" />
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="tags">Tags</label>
+                <v-select
+                    multiple
+                    :options="tags"
+                    :taggable="true"
+                    v-model="todo.tags"
+                    label="title"></v-select>
+            </div>
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea
+                    id="description"
+                    class="form-control"
+                    rows="3"
+                    v-model="todo.description"></textarea>
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="start">Start</label>
+                <datepicker
+                    name="start"
+                    format="dd/MM/yyyy"
+                    input-class="form-control datepicker__input"
+                    language="pt-br"
+                    :required="true"
+                    :disabled="startOptions"
+                    @selected="handleStartChange"
+                    v-model="todo.start"></datepicker>
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="end">End</label>
+                <datepicker
+                    name="end"
+                    format="dd/MM/yyyy"
+                    input-class="form-control datepicker__input"
+                    language="pt-br"
+                    :required="true"
+                    :disabled="endOptions"
+                    @selected="handleEndChange"
+                    v-model="todo.end"></datepicker>
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" v-model="todo.done"> Done
+                </label>
+            </div>
+            <button
+                type="button"
+                class="btn btn-primary"
+                v-if="!isEditMode"
+                @click.prevent="addTodo()">Submit</button>
+            <button
+                type="button"
+                class="btn btn-primary"
+                v-if="isEditMode"
+                @click.prevent="editTodo()">Submit</button>
+            <button
+                type="button"
+                class="btn btn-danger"
+                v-if="isEditMode"
+                @click="removeTodo()">Remove</button>
+            <button
+                type="button"
+                class="btn btn-default"
+                v-if="isEditMode"
+                @click="cancelEditMode()">Cancel</button>
+        </form>
+        <simplert :useRadius="true"
+            :useIcon="true"
+            ref="simplert">
+        </simplert>
+    </div>
 </template>
 
 <script>
@@ -84,9 +90,10 @@ import { mapGetters } from 'vuex';
 import Datepicker from 'vuejs-datepicker';
 import moment from 'moment';
 import vSelect from 'vue-select';
+import Simplert from 'vue2-simplert';
 
 export default {
-    components: { Datepicker, vSelect },
+    components: { Datepicker, vSelect, Simplert },
     computed: {
         ...mapGetters({
             todo: 'getTodoInit',
@@ -117,7 +124,19 @@ export default {
             });
         },
         removeTodo() {
-            this.$store.dispatch('TODO_REMOVE', this.todo);
+            this.$refs.simplert.openSimplert({
+                title: 'Atenção',
+                message: 'Deseja remover este registro?',
+                type: 'warning',
+                useConfirmBtn: true,
+                customCloseBtnText: 'Cancel',
+                customCloseBtnClass: 'simplert__close simplert__close--radius',
+                customConfirmBtnText: 'Ok',
+                customConfirmBtnClass: 'simplert__confirm simplert__confirm--radius',
+                onConfirm: () => {
+                    this.$store.dispatch('TODO_REMOVE', this.todo);
+                }
+            });
         },
         handleStartChange(date) {
             this.$store.commit('FORM_CHANGE_START_OPTIONS', { date });
